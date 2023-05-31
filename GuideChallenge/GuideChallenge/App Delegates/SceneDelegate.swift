df01fd76c676ee8db777c5b55b73edb45accb38f
Joseph Pecoraro <jpecoraro342@yahoo.com>
2023-05-30 20:56:47 -0400

    Initial Commit

---
+++ GuideChallenge/GuideChallenge/App Delegates/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  GuideChallenge
+//
+//  Created by Joseph Pecoraro on 5/30/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+                window.rootViewController = GuideListVC()
+                self.window = window
+                window.makeKeyAndVisible()
+    }
+}
+
---
+++ GuideChallenge/GuideChallenge/GuideList/GuideListVC.swift
@@ -0,0 +1,19 @@
+//
+//  GuideListVC.swift
+//  GuideChallenge
+//
+//  Created by Joseph Pecoraro on 5/30/23.
+//
+
+import UIKit
+
+class GuideListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
